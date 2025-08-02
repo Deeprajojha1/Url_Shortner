a8141bc57b286897f02b2ddf5b8c3cbe54c306a6
Deepraj Ojha <deeprajojha2022@gmail.com>
2025-08-03 01:03:45 +0530

    Make node_module
    1: in your terminal you install npm i express ejs mongoose
    2: in your terminal you install npm init -y

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { shortUrl,redirectUrl } from './controllers/url.js'; // Adjust path if needed
+
+const app = express(); // <-- Move this line up
+
+// Middleware to parse request body
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb+srv://ojhadeepraj70:NKQnsAQ7811sU1dY@cluster0.hgkanxg.mongodb.net/', { dbName: "Mongodb_connection" })
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
+//   Rendering the ejs file
+app.get('/', (req, res) => {
+    res.render('index.ejs', { shortenedUrl: null });
+});
+// POST route to handle URL shortening
+app.post('/shorten', shortUrl);
+
+// redirect to original URL
+app.get('/:shortCode', redirectUrl);
+
+const port = 1000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
